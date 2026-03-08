029b911837148b37e7941ac633733c643b492653
kyowshi-dev <chsa.chavaria.coc@phinmaed.com>
2026-03-08 21:02:23 +0800

    Add card creation and evaluation functions
    - Implement createCard function to create a card object with id, question, answers, and correctAnswer properties.
    - Implement evaluateGuess function to check if a guess matches the correct answer and return feedback.
    - Update tests to verify the functionality of createCard and evaluateGuess, including tests for correct and incorrect guesses.

--- test/card-test.js
+++ test/card-test.js
@@ -1,19 +1,37 @@
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
 
-const { createCard } = require('../src/card');
+const { createCard, evaluateGuess } = require("../src/card");
 
-describe('card', function() {
-  it.skip('should be a function', function() {
-    expect(createCard).to.be.a('function');
+describe("card", function () {
+  it("should be a function", function () {
+    expect(createCard).to.be.a("function");
   });
 
-  it.skip('should create a card and its properties', function() {
-    const card = createCard(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-    
+  it("should create a card and its properties", function () {
+    const card = createCard(
+      1,
+      "What allows you to define a set of related information using key-value pairs?",
+      ["object", "array", "function"],
+      "object",
+    );
+
     expect(card.id).to.equal(1);
-    expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
-    expect(card.answers).to.deep.equal(['object', 'array', 'function']);
-    expect(card.correctAnswer).to.equal('object');
-  });  
+    expect(card.question).to.equal(
+      "What allows you to define a set of related information using key-value pairs?",
+    );
+    expect(card.answers).to.deep.equal(["object", "array", "function"]);
+    expect(card.correctAnswer).to.equal("object");
+  });
+
+  // it("should evaluate a guess as correct or incorrect", function () {
+  it('should return "correct!" if the guess is right', function () {
+    const feedback = evaluateGuess("object", "object");
+    expect(feedback).to.equal("correct!");
+  });
+
+  it('should return "incorrect!" if the guess is wrong', function () {
+    const feedback = evaluateGuess("array", "object");
+    expect(feedback).to.equal("incorrect!");
+  });
 });
